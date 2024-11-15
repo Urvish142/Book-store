7a770a543fae9b114268bbc1abae2087329f2f64
Urvish142 <urvishmehta18@gmail.com>
2024-11-16 00:58:30 +0530

    MERN PROJECT

---
+++ backend/config.js
@@ -0,0 +1,3 @@
+export const PORT = 5555;
+
+export const mongoDBURL = 'mongodb+srv://urvishmehta18:xv22QWrVMcTWchXJ@bookstore.gm3po.mongodb.net/book-collection?retryWrites=true&w=majority&appName=bookstore'
